dc5bf00083cb01d5fd1bc5b0ba9396c8b89cc223
Elijah Carter <ecarter1011@gmail.com>
2026-03-08 11:57:11 -0700

    Inital commit

---
+++ sketch_mar8a/sketch_mar8a.ino
@@ -0,0 +1,18 @@
+int whiteLED1 = 12;
+int greenLED = 11;
+int yellowLED = 10;
+int redLED = 9;
+int whiteLED2 = 8;
+
+int buzzer = 7;
+
+int button1 = 13;
+int button2 = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
